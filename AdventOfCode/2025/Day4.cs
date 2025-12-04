eb9b9a6f8b06badcaf45691b780f4565e8baa153
Pranav Senthilnathan <pranav.senthilnathan@live.com>
2025-12-04 02:57:23 -0800

    2025 day 4

---
+++ AdventOfCode/2025/Day4.cs
@@ -0,0 +1,65 @@
+using AdventOfCode.lib;
+
+namespace AdventOfCode.Year2025;
+
+internal static class Day4
+{
+    [AnswerMethod(2025, 4, 1)]
+    public static string Part1(string[] input)
+    {
+        var ans = 0;
+        var grid = input.Select(line => line.Trim()).ToArray().CreateGrid();
+
+        foreach (var node in grid)
+        {
+            if (node.Value != '@') continue;
+
+            var numNeighbors = 0;
+            foreach (var neighbor in node.Key.GetAllNeighbors())
+            {
+                if (grid.TryGetValue(neighbor, out var n) && n == '@')
+                {
+                    numNeighbors++;
+                }
+            }
+
+            if (numNeighbors < 4) ans++;
+        }
+
+        return ans.ToString();
+    }
+
+    [AnswerMethod(2025, 4, 2)]
+    public static string Part2(IEnumerable<string> input)
+    {
+        var grid = input.Select(line => line.Trim()).ToArray().CreateGrid();
+        var cnt = grid.Count;
+        bool removed;
+        do
+        {
+            removed = false;
+
+            foreach (var node in grid)
+            {
+                if (node.Value != '@') continue;
+
+                var numNeighbors = 0;
+                foreach (var neighbor in node.Key.GetAllNeighbors())
+                {
+                    if (grid.TryGetValue(neighbor, out var n) && n == '@')
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                if (numNeighbors < 4)
+                {
+                    grid.Remove(node.Key);
+                    removed = true;
+                }
+            }
+        } while (removed);
+
+        return (cnt - grid.Count).ToString();
+    }
+}
